5bce8ba28623111136976acd4cd72fccb0aadde5
Philipp Moser <philipp.moser@finanzen.net>
2025-05-22 16:20:35 +0200

    PMO: Setup Custom Plugin to determine the current Date and Time

---
+++ SemanticKernelPlugins/Plugins/TimeInformationPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace SemanticKernelPlugins.Plugins;
+
+public class TimeInformationPlugin
+{
+    [KernelFunction]
+    [Description("Retrieves the current time and day in CET from germany.")]
+    public string GetCurrentTimeAndDay()
+    {
+        var cetZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+        var germanTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cetZone);
+
+        var time = germanTime.ToString("HH:mm:ss");
+        var dayOfWeek = germanTime.ToString("dddd", new System.Globalization.CultureInfo("de-DE"));
+
+        return $"Current time in germany: {time}, day: {dayOfWeek}";
+    }
+}
--- SemanticKernelPlugins/Program.cs
+++ SemanticKernelPlugins/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using OllamaApiFacade.Extensions;
+using SemanticKernelPlugins.Plugins;
 
 var builder = WebApplication
                     .CreateBuilder(args)
                     .ConfigureAsLocalOllamaApi();
 
-builder.Services.AddKernel().AddLmStudio();
+builder.Services
+    .AddKernel()
+    .AddLmStudio()
+    .Plugins.AddFromType<TimeInformationPlugin>();
 
 var app = builder.Build();
 
@@ -12,8 +18,13 @@ app.MapPostApiChat(async (chatRequest, chatCompletionService, httpContext, kerne
 {
     var chatHistory = chatRequest.ToChatHistory();
 
+    var promptExecutionSettings = new OpenAIPromptExecutionSettings
+    {
+        FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+    };
+
     await chatCompletionService
-        .GetStreamingChatMessageContentsAsync(chatHistory)
+        .GetStreamingChatMessageContentsAsync(chatHistory, promptExecutionSettings, kernel)
         .StreamToResponseAsync(httpContext.Response);
 });
 
